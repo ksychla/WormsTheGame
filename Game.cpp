57344a93bd9f3c38071af730c8d28aeca80d8b61
Krzysztof Sychla <ksychla5@gmail.com>
2019-06-23 20:05:23 +0200

    Terrain collisions

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Krzysztof Sychla on 2019-06-23.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by Krzysztof Sychla on 2019-06-23.
+//
+
+#ifndef OPENGLSETUP_GAME_H
+#define OPENGLSETUP_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //OPENGLSETUP_GAME_H
